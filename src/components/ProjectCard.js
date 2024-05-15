d74467088e6ae8f0e22ec921153051c71dccb195
kotsiossp97 <kotsiossp@gmail.com>
2024-05-15 14:24:51 +0300

    Updated projects cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,37 +1,76 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material'
-import React from 'react'
-import ProjectCodingImg from '../static/projectCoding.jpg'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ProjectCodingImg from "../static/projectCoding.jpg";
 
 const ProjectCard = (props) => {
-    const project = props.project
-    const img = project?.image?project.image:ProjectCodingImg
+  const project = props.project;
+  const img = project?.image ? project.image : ProjectCodingImg;
 
-    const handleButtonClick = () => {
-        window.open(project.link, "_blank")
-    }
-    return (
-        <Card sx={{ display: 'flex', height: "100%",  justifyContent: "space-between"}} raised >
-            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography variant='h5' fontWeight="bold" >{project.title}</Typography>
-                    <Typography variant='subtitle2' gutterBottom>{project.date}</Typography>
-                    <Typography >{project.description}</Typography>
-                </CardContent>
-                <CardActions>
-                    {
-                        project.link !== "" &&
-                        <Button onClick={handleButtonClick}>See More</Button>
-                    }
-                </CardActions>
-            </Box>
-            <CardMedia  
-                component="img"
-                sx={{ width: "40%" }} 
-                image={img} />
-        </Card>
-    )
-}
+  const handleButtonClick = () => {
+    window.open(project.link, "_blank");
+  };
+  return (
+    <Card
+      sx={{
+        display: "flex",
+        // height: "100%",
+        justifyContent: "space-between",
+        flexDirection: {
+          xs: "column-reverse",
+          sm: "column-reverse",
+          md: "row",
+        },
+      }}
+      raised
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", flex: 2 }}>
+        <CardContent sx={{ flex: "1" }}>
+          <Typography variant="h5" fontWeight="bold">
+            {project.title}
+          </Typography>
+          <Typography variant="subtitle2" gutterBottom>
+            {project.date}
+          </Typography>
+          <Typography>{project.description}</Typography>
+        </CardContent>
+        <CardActions>
+          {project.link !== "" && (
+            <Button onClick={handleButtonClick}>See More</Button>
+          )}
+        </CardActions>
+      </Box>
+      <Box
+        sx={{
+          height: {
+            xs: "200px",
+            md: "auto",
+          },
+          width: {
+            sm: "100%",
+            md: "max(30%, 200px)",
+          },
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{
+            aspectRatio: "1",
+            width: "100%",
+            height: "100%",
+          }}
+          image={img}
+        />
+      </Box>
+    </Card>
+  );
+};
 
-
-
-export default ProjectCard
+export default ProjectCard;
--- src/static/MyProjects.js
+++ src/static/MyProjects.js
@@ -1,35 +1,57 @@
-import PythonImg from "../static/python.png"
-import LPMCD from "../static/lmcd.jpg"
-import FS22Dir from "../static/fs22DirChanger.png"
+import PythonImg from "../static/python.png";
+import LPMCD from "../static/lmcd.jpg";
+import FS22Dir from "../static/fs22DirChanger.png";
+import CafeScreen from "../static/cafeScreenshot.png";
 
 export const MyProjects = [
-    {
-        title: "Low Processing Monitoring and Control Device",
-        description: "During the senior year of my studies, I implemented a monitoring and control web application, for photovoltaic (PV) systems.",
-        link: "",
-        date: "May 20, 2021",
-        image: LPMCD
-    },
-    {
-        title: "FS22 Mod Directory Changer",
-        description: "A small app developed using React and Electron. It's purpose is to handle the directory of the mods for the game Farming Simulator 22.",
-        link: "https://github.com/kotsiossp97/fs22-mod-directory-changer",
-        date: "April 22, 2023",
-        image: FS22Dir
-    },
-    {
-        title: "pyFolderOrganizer",
-        description: "Python package to organize a folder in sub-directories based on file types. It's currently published as a package in PyPI",
-        link: "https://github.com/kotsiossp97/pyFolderOrganizer",
-        date: "May 28, 2023",
-        image: PythonImg
-    },
-    {
-        title: "Portfolio Webpage",
-        description: "As part of my React learning path, I've decided to build my portfolio website using ReactJS framework. It is the current page you are viewing😋.",
-        link: "https://github.com/kotsiossp97/kotsiossp97.github.io",
-        date: "July 03, 2023"
-    }
-]
+  {
+    title: "Low Processing Monitoring and Control Device",
+    description:
+      "During the senior year of my studies, I implemented a monitoring and control web application, for photovoltaic (PV) systems.",
+    link: "",
+    date: "May 20, 2021",
+    image: LPMCD,
+  },
+  {
+    title: "FS22 Mod Directory Changer",
+    description:
+      "A small app developed using React and Electron. It's purpose is to handle the directory of the mods for the game Farming Simulator 22.",
+    link: "https://github.com/kotsiossp97/fs22-mod-directory-changer",
+    date: "April 22, 2023",
+    image: FS22Dir,
+  },
+  {
+    title: "pyFolderOrganizer",
+    description:
+      "Python package to organize a folder in sub-directories based on file types. It's currently published as a package in PyPI",
+    link: "https://github.com/kotsiossp97/pyFolderOrganizer",
+    date: "May 28, 2023",
+    image: PythonImg,
+  },
+  {
+    title: "Portfolio Webpage",
+    description:
+      "As part of my React learning path, I've decided to build my portfolio website using ReactJS framework. It is the current page you are viewing😋.",
+    link: "https://github.com/kotsiossp97/kotsiossp97.github.io",
+    date: "July 03, 2023",
+  },
+  {
+    title: "Cafe Agios Konstantinos Web App",
+    description:
+      "A small React SPA application for a local cafe called Agios Konstantinos. The application is providing the menu and details of the cafe.",
+    link: "https://cafe-agioskonstantinos.web.app",
+    date: "November 10, 2023",
+    image: CafeScreen,
+  },
+  {
+    title: "SimRacing FOV Calculator",
+    description:
+      "A simple React Application for calculating the correct FOV setting for various sim racing games.",
+    link: "https://github.com/kotsiossp97/simracing-fov-calculator/",
+    date: "March 15, 2024",
+    image:
+      "https://raw.githubusercontent.com/kotsiossp97/simracing-fov-calculator/main/screenshots/screen1.png",
+  },
+];
 
 // "https://raw.githubusercontent.com/kotsiossp97/fs22-mod-directory-changer/master/screenshots/screen1.png"
